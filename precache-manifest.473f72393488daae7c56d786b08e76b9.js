b3fffe9e862c68db6e80a3935c888a5842472b67
Quan Huynh <qlhuynh@gmail.com>
2020-03-19 18:26:18 -0700

    Updates

---
+++ precache-manifest.473f72393488daae7c56d786b08e76b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c608add2f85fbac28b64d9dc4761f72",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "38b15c2ff328d5e47f99",
+    "url": "/react-calculator/static/js/2.51e2010b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-calculator/static/js/2.51e2010b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb46a15b1d797a3e5d5",
+    "url": "/react-calculator/static/js/main.ffaa66fb.chunk.js"
+  },
+  {
+    "revision": "c7943d3ad8432cdf3d7d",
+    "url": "/react-calculator/static/js/runtime-main.d9df8da5.js"
+  }
+]);
